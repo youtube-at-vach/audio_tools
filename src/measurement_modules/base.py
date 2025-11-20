2a0e3472a6e256a93dd90bb0ef327e0428af4840
バーチャ農ちゃんねる <vach.catering235@passmail.net>
2025-11-20 14:19:13 +0900

    deleted:    legacy/misc/noise_calc.py 	deleted:    legacy/misc/noise_calc_i.py 	modified:   src/measurement_modules/base.py 	debug_device.py 	diagnose_audio.py 	find_loopback_input.py 	main_gui.py 	requirements.txt 	src/core/ 	src/gui/

--- src/measurement_modules/base.py
+++ src/measurement_modules/base.py
@@ -15,3 +15,11 @@ class MeasurementModule(ABC):
     @abstractmethod
     def run(self, args: argparse.Namespace):
         pass
+
+    def get_widget(self):
+        """
+        Returns a QWidget instance for the GUI.
+        Override this method to provide a custom GUI for the module.
+        """
+        return None
+
